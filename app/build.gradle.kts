465cf29e617092f201d2e758fd7f670e448aad72
YoussoufSid <sidibey487@gmail.com>
2026-09-12 23:07:52 +0000

    Fix Kotlin plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("org.jetbranins.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.android") version "2.1.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
     id("com.google.gms.google-services")
 }
 
